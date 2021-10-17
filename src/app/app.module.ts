4f3d9a74c0c3d135e902ecb254e01635fb0a90ea
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-10-17 18:02:15 +0300

    add form component and repository explanation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsComponent } from './forms/forms.component';
+import { GithubExplanationComponent } from './github-explanation/github-explanation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent,
+    GithubExplanationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/github-explanation/github-explanation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-github-explanation',
+  templateUrl: './github-explanation.component.html',
+  styleUrls: ['./github-explanation.component.css']
+})
+export class GithubExplanationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
